bfdcf7da2e38fbcd6ff6d818d0289e17ae499075
Andrii Nakryiko <andriin@fb.com>
2019-09-27 20:13:30 -0700

    bpf: track contents of read-only maps as scalars
    Maps that are read-only both from BPF program side and user space side
    have their contents constant, so verifier can track referenced values
    precisely and use that knowledge for dead code elimination, branch
    pruning, etc.
    
    Signed-off-by: Andrii Nakryiko <andriin@fb.com>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -2739,6 +2739,12 @@ static void coerce_reg_to_size(struct bpf_reg_state *reg, int size)
 	reg->smax_value = reg->umax_value;
 }
 
+static bool bpf_map_is_rdonly(const struct bpf_map *map)
+{
+	return (map->map_flags & BPF_F_RDONLY_PROG) &&
+	       ((map->map_flags & BPF_F_RDONLY) || map->frozen);
+}
+
 /* check whether memory at (regno + off) is accessible for t = (read | write)
  * if t==write, value_regno is a register which value is stored into memory
  * if t==read, value_regno is a register which will receive the value from memory
@@ -2776,9 +2782,28 @@ static int check_mem_access(struct bpf_verifier_env *env, int insn_idx, u32 regn
 		if (err)
 			return err;
 		err = check_map_access(env, regno, off, size, false);
-		if (!err && t == BPF_READ && value_regno >= 0)
-			mark_reg_unknown(env, regs, value_regno);
+		if (!err && t == BPF_READ && value_regno >= 0) {
+			struct bpf_map *map = reg->map_ptr;
 
+			/* if map is read-only, track scalars precisely */
+			if (tnum_is_const(reg->var_off) &&
+			    bpf_map_is_rdonly(map) &&
+			    map->ops->map_direct_value_addr) {
+				int map_off = off + reg->var_off.value;
+				u64 val = 0, ptr;
+
+				err = map->ops->map_direct_value_addr(map, &ptr,
+								      map_off);
+				if (err)
+					return err;
+
+				memcpy(&val, (void *)ptr, size);
+				regs[value_regno].type = SCALAR_VALUE;
+				__mark_reg_known(&regs[value_regno], val);
+			} else {
+				mark_reg_unknown(env, regs, value_regno);
+			}
+		}
 	} else if (reg->type == PTR_TO_CTX) {
 		enum bpf_reg_type reg_type = SCALAR_VALUE;
 
